434025acd89d0c32f6949ce3e4b3d486058efba1
Idriss Nguepi <idrissnguepi@yahoo.fr>
2023-05-03 23:56:08 +0000

    my commit of the day

--- .ipynb_checkpoints/preprocess3-checkpoint.sh
+++ .ipynb_checkpoints/preprocess3-checkpoint.sh
@@ -4,5 +4,5 @@ AFROMT_PATH=$2
 for i in train valid test; do for j in ln ; do python /home/jupyter/afromt/training/scripts/spm_encode.py --model=$AFROMT_PATH/spm_model.model < $i.$j > $i.spm.$j & done; done
 
 fairseq-preprocess \
-	--trainpref train.spm --validpref valid.spm --testpref test.spm --destdir data-bin --workers=80 --source-lang ln --only-source --bpe sentencepiece --srcdict $AFROMT_PATH/fairseq.vocab 
+	--trainpref train.spm --validpref valid.spm --testpref test.spm --destdir data-bin-afromt --workers=80 --source-lang ln --only-source --bpe sentencepiece --srcdict $AFROMT_PATH/fairseq.vocab 
 
